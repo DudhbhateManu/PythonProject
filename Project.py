0951ee4a1bdb4d481cb3a1d076e569dc2584c9d1
Manisha Dudhbhate <122846622+DudhbhateManu@users.noreply.github.com>
2023-03-06 23:53:56 +0530

    Add files via upload

---
+++ Project.py
@@ -0,0 +1,76 @@
+from flask import Flask
+from tkinter import *
+from tkinter import ttk
+from googletrans import Translator, LANGUAGES
+
+app=Flask(__name__)
+
+@app.route("/change")
+def change(text="type", src="English", dest="Hindi"):
+    text1=text
+    src1=src
+    dest1=dest
+    trans=Translator()
+    trans1=trans.translate(text,src=src1,dest=dest1)
+    return trans1.text
+
+@app.route("/data")
+def data():
+    s=comb_sor.get()
+    d=comb_dest.get()
+    masg=Sor_txt.get(1.0,END)
+    textget=change(text=masg, src=s, dest=d)
+    des_txt.delete(1.0,END)
+    des_txt.insert(END,textget)
+
+
+
+root=Tk()
+root.title("Translator")
+root.geometry("2000x1200")
+
+ph=PhotoImage(file="C:\\Python311\\background3.png")
+label=ttk.Label(root, image=ph)
+PhotoImage(file="C:\\Python311\\background3.png")
+label.pack()
+
+lab_txt=Label(root,text="Translator" ,font=("Time New roman", 40,"bold"))
+lab_txt.place(x=630,y=40,height=50,width=300)
+
+frame=Frame(root).pack(side=BOTTOM)
+
+lab_txt=Label(root,text="Source text", font=("Time New roman", 20,"bold"),fg="Black",bg="green")
+lab_txt.place(x=630,y=130,height=20,width=300)
+
+Sor_txt=Text(frame, font=("Time New roman", 20,"bold"),wrap=WORD)
+Sor_txt.place(x=530,y=190,height=150,width=480)
+
+list_text=list(LANGUAGES.values())
+
+comb_sor=ttk.Combobox(frame,value=list_text)
+comb_sor.place(x=520,y=400,height=40,width=150)
+comb_sor.set("English")
+
+button_change=Button(frame,text="Translate", relief=RAISED, command=data)
+button_change.place(x=690,y=400,height=40,width=160)
+
+comb_dest=ttk.Combobox(frame,value=list_text)
+comb_dest.place(x=870,y=400,height=40,width=150)
+comb_dest.set("English")
+
+lab_txt=Label(root,text="Dest text", font=("Time New roman", 20,"bold"),fg="Black",bg="green")
+lab_txt.place(x=630,y=500,height=20,width=300)
+
+des_txt=Text(frame, font=("Time New roman", 20,"bold"),wrap=WORD)
+des_txt.place(x=530,y=550,height=150,width=480)
+root.mainloop()
+
+if __name__== '__main__':
+    app.run(debug=True)
+
+
+
+
+
+
+
